3136d63983110cbde827f00aab47746b65699c13
Andrejs Logins <andrejslogins@gmail.com>
2017-11-02 14:07:08 +0200

    md3 changes

--- MD3/main.c
+++ MD3/main.c
@@ -17,18 +17,6 @@ typedef struct file {
     struct file *prev;
 }file;
 
-typedef struct identicalFileList{
-    file *first;
-    file *last;
-
-    struct identicalFileList *next;
-    struct identicalFileList *prev;
-} identicalFileList;
-
-identicalFileList *firstList = NULL;
-identicalFileList *lastList = NULL;
-
-
 file *first = NULL;
 file *last = NULL;
 
@@ -40,13 +28,6 @@ void setFileValues(file *file){
     file->filePath = NULL;
     file->fileSize = NULL;
 }
-
-void setFileListValues(identicalFileList *node){
-    node->next=NULL;
-    node->first=NULL;
-    node->last=NULL;
-}
-
 
 int isRelativeDirPath(char *value){
     if(value[0] == '.'){
@@ -93,15 +74,17 @@ void readDir(char *string) {
     if((dir = opendir(string)) != NULL){
         struct dirent *dirp;
         while((dirp = readdir(dir)) != NULL){
-                char *currDir = malloc(sizeof(char) * 255 );
-                sprintf(currDir, "%s/%s", string, dirp->d_name);
+            char *currDir = malloc(sizeof(char) * 255 );
+            sprintf(currDir, "%s/%s", string, dirp->d_name);
             if(dirp->d_type == DT_DIR && !isRelativeDirPath(dirp->d_name)){
                 readDir(currDir);
             } else if (dirp->d_type != DT_DIR) {
-                struct stat statAction;
-                stat(currDir, &statAction);
-                char *md5HashValue = getMD5Hash(currDir);
-                fileListAppend(currDir, statAction.st_size, statAction.st_mtim, md5HashValue,dirp->d_name);
+                struct stat lstatAction;
+                lstat(currDir, &lstatAction);
+                if(lstatAction.st_mode != S_IFLNK) {
+                    char *md5HashValue = getMD5Hash(currDir);
+                    fileListAppend(currDir, lstatAction.st_size, lstatAction.st_mtim, md5HashValue,dirp->d_name);
+                }
             }
         }
         closedir(dir);
@@ -135,7 +118,9 @@ void printDirs(){
     file *curr;
 //    for ()
     for (curr = first; curr != NULL; curr = curr->next) {
-        printf("%d %d %s %s\n",curr->fileSize,curr->lastEdited.tv_nsec, curr->fileMD5Hash ,curr->filePath);
+//        printf("%d %d %s %s %s\n",curr->fileSize,curr->lastEdited.tv_nsec, curr->fileMD5Hash ,curr->filePath, curr->fileName);
+        printf("%s %s\n",curr->filePath, curr->fileName);
+//        printf("%s\n",curr->filePath);
     }
 }
 
@@ -167,110 +152,73 @@ void printHelp(){
     printf("-h izvada paliga tekstu par parametriem\n");
 }
 
-file *createFileCopy(file *node){
-    file *newNode = malloc(sizeof(file));
-//    memcpy(newNode, node, sizeof(file));
-    newNode->fileMD5Hash = node->fileMD5Hash;
-    newNode->lastEdited = node->lastEdited;
-    newNode->fileSize = node->fileSize;
-    newNode->fileName = node->fileName;
-    newNode->filePath = node->filePath;
-    newNode->next = NULL;
-    newNode->prev = NULL;
-    return newNode;
-}
+void printUnique(int state){
+    file *outerLoop;
+    file *innerLoop;
+    file *temp;
+    int foundEqual;
+    for(outerLoop = first;outerLoop != NULL; outerLoop = outerLoop->next){
+        foundEqual = 0;
+        for(innerLoop = first->next; innerLoop != NULL; innerLoop = innerLoop->next){
+            switch(state){
+                case 0:
+                    if(
+//                            strcmp(&outerLoop->fileName,&innerLoop->fileName) ==0
+//                       && outerLoop->fileSize == innerLoop->fileSize){
+                       outerLoop->fileSize == innerLoop->fileSize){
+                        if (foundEqual == 1) {
+                            printf("%s\n",outerLoop->filePath);
+                            if(innerLoop->next != NULL){
+                                innerLoop->prev->next = innerLoop->next;
+                                innerLoop->next->prev = innerLoop->prev;
+                                innerLoop = innerLoop->prev;
+                            } else {
+                                innerLoop->prev->next = NULL;
+                            }
+                        }
+                        if(foundEqual == 0){
+                            printf("=======================\n");
+                            printf("%s\n", innerLoop->filePath);
+                            foundEqual = 1;
+                        }
+//                        temp = innerLoop->prev;
+                    }
+                    break;
 
-void addItemToList(identicalFileList *list, file *node){
-    if(list->first == NULL){
-        list->first = node;
-        list->last = node;
-    } else if(list->first == list->last){
-        list->last = node;
-        list->first->next = node;
-        list->last->prev = list->first;
-    } else {
-        list->last->next = node;
-        node->prev = list->last;
-        list->last = node;
-    }
-}
+                case 1:
+                    if(strcmp(&outerLoop->fileName,&innerLoop->fileName) == 0
+                       && outerLoop->fileSize == innerLoop->fileSize
+                       && outerLoop->lastEdited.tv_sec == innerLoop->lastEdited.tv_sec
+                       && outerLoop->lastEdited.tv_nsec == innerLoop->lastEdited.tv_nsec){
+                        printf("%s\n",innerLoop->filePath);
+                    }
+                    break;
 
-identicalFileList *findIdenticalFileList(file *fileNode, int state){
-    identicalFileList *currList;
-    for(currList = firstList;currList != NULL; currList = currList->next){
-        switch(state){
-            case 0:
-                if(currList->first->fileName == fileNode->fileName
-                   && currList->first->fileSize == fileNode->fileSize){
-                    return currList;
-                }
-                break;
+                case 2:
+                    if(strcmp(&outerLoop->fileMD5Hash,&innerLoop->fileMD5Hash) == 0){
+                        printf("%s\n",innerLoop->filePath);
+                    }
+                    break;
 
-            case 1:
-                if(currList->first->fileName == fileNode->fileName
-                   && currList->first->fileSize == fileNode->fileSize
-                   && currList->first->lastEdited.tv_sec == fileNode->lastEdited.tv_sec){
-                    return currList;
-                }
-                break;
-
-            case 2:
-                if(currList->first->fileMD5Hash == fileNode->fileMD5Hash){
-                    return currList;
-                }
-                break;
-
-            case 3:
-                if(currList->first->fileName == fileNode->fileName
-                   && currList->first->fileSize == fileNode->fileSize
-                   && currList->first->lastEdited.tv_sec == fileNode->lastEdited.tv_sec
-                   && currList->first->fileMD5Hash == fileNode->fileMD5Hash){
-                    return currList;
-                }
-                break;
+                case 3:
+                    if(strcmp(&outerLoop->fileName,&innerLoop->fileName) == 0
+                       && outerLoop->fileSize == innerLoop->fileSize
+                       && outerLoop->lastEdited.tv_sec == innerLoop->lastEdited.tv_sec
+                       && outerLoop->lastEdited.tv_nsec == innerLoop->lastEdited.tv_nsec
+                       && strcmp(&outerLoop->fileMD5Hash,&innerLoop->fileMD5Hash) == 0){
+                        printf("%s\n",innerLoop->filePath);
+                    }
+                    break;
+            }
         }
     }
-    return NULL;
 }
 
-void processFiles(int state){
 //    States:
 //    0 : name/size
 //    1 : name/size/date
 //    2 : MD5 hash
 //    3 : MD5 hash/name/size/date
-    //                    addItemToList(currList, fileNode);
-    file *fileNode;
-    for(fileNode=first;fileNode != NULL;fileNode=fileNode->next){
-        identicalFileList *list = findIdenticalFileList(fileNode,state);
-        if(list != NULL) {
-            file *passedNode = createFileCopy(fileNode);
-            addItemToList(list,passedNode);
-        } else {
-            identicalFileList *fileList;
-            fileList = malloc(sizeof(fileList));
-            setFileListValues(fileList);
-            if(firstList == NULL ){
-                firstList = fileList;
-                lastList = fileList;
-                file *passedNode = createFileCopy(fileNode);
-                addItemToList(list,passedNode);
-            } else if (firstList == lastList){
-                firstList->next = fileList;
-                lastList = fileList;
-                lastList->prev = firstList;
-                file *passedNode = createFileCopy(fileNode);
-                addItemToList(list,passedNode);
-            } else {
-                lastList->next = fileList;
-                fileList->prev = lastList;
-                lastList = fileList;
-                file *passedNode = createFileCopy(fileNode);
-                addItemToList(list,passedNode);
-            }
-        }
-    }
-}
 
 int main(int argc, char *argv[]) {
     readDir(".");
@@ -293,16 +241,17 @@ int main(int argc, char *argv[]) {
         }
 
         if(containsM && containsD){
-            processFiles(3);
+            printUnique(3);
         } else if(containsD) {
-            processFiles(1);
+            printUnique(1);
         } else if(containsM) {
-            processFiles(2);
+            printUnique(2);
         } else {
             printHelp();
         }
     } else {
-        processFiles(0);
+//        printUnique(0);
+        printDirs();
         return 0;
     }
     return 0;
